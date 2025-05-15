62a2b3f825ec259d26111e74c52a0cc803a401d6
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-15 20:27:58 -0300

    Alterando função de logout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
+/* CARREGAR DICIONARIO DE CADASTRO NO LOCAL STORAGE */
+document.addEventListener("DOMContentLoaded", function () {
+  dicionario_cadastros();
+});
+
 /* CRIAR DICIONARIO CADASTROS */
 function dicionario_cadastros() {
   const cadastros = [
-    { id: 1, usuario: "Clarice", senha: "1111" },
-    { id: 2, usuario: "Daniel", senha: "2222" },
-    { id: 3, usuario: "Guilherme", senha: "3333" },
-    { id: 4, usuario: "Mateus", senha: "4444" },
+    { id: 1, usuario: "Clarice", login: "Cla", senha: "1111" },
+    { id: 2, usuario: "Daniel", login: "Dan", senha: "2222" },
+    { id: 3, usuario: "Guilherme", login: "Gui", senha: "3333" },
+    { id: 4, usuario: "Mateus", login: "Mat", senha: "4444" },
   ];
   /* PASSAR PARA O FORMATO JSON */
   const meujson = JSON.stringify(cadastros);
@@ -16,24 +21,28 @@ function dicionario_cadastros() {
 function logar() {
   const login = document.querySelector("#login").value;
   const senha = document.querySelector("#senha").value;
+  /* PEGAR DADOS DO LOCAL STORAGE */
   const dados = JSON.parse(localStorage.getItem("bancodados"));
+  /* VERIFICAR LOGIN E SENHA */
   for (let i = 0; i < dados.length; i++) {
-    if (login == dados[i].usuario && senha == dados[i].senha) {
-      alert(`Bem vindo: ${dados[i].usuario}`);
-      sessionStorage.setItem("usuarioLogado", dados[i].usuario);
-      window.location.href = "index.html";
+    if (login == dados[i].login && senha == dados[i].senha) {
+      alert(`Bem vindo: ${dados[i].usuario}`); // DAR BOAS VINDAS AO CLIENTE
+      sessionStorage.setItem("usuarioLogado", dados[i].usuario); // GUARDAR INFORMACAO DE LOGADO
+      window.location.href = "index.html"; // DIRECIONAR PARA PAGINA INICIAL DEPOIS DE LOGADO
     }
   }
 }
 
 /* VERIFICA SE ESTÁ LOGADO E MUDA O NOME NO HEADER */
-const linkLoginElement = document.getElementById("link_login");
+const link_login = document.getElementById("link_login");
 const usuario = sessionStorage.getItem("usuarioLogado");
 if (usuario) {
-  linkLoginElement.textContent = "Deslogar";
-  linkLoginElement.href = "#";
-  linkLoginElement.onclick = () => {
-    deslogar();
+  link_login.textContent = usuario;
+  link_login.href = "#";
+  link_login.onclick = () => {
+    if (confirm(usuario + " Você está logado, deseja sair?")) {
+      deslogar();
+    }
   };
 }
 
@@ -43,4 +52,5 @@ function deslogar() {
   alert("Você foi desconectado.");
   window.location.reload();
 }
+
 
