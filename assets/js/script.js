906ecabfb31255ece8d41e86619147da69edb690
GuilhermeSilva <guilherme.silva672@fatec.sp.gov.br>
2025-05-17 16:46:38 -0300

    Criando dicionario de produtos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,98 @@
 /* CARREGAR DICIONARIO DE CADASTRO NO LOCAL STORAGE */
 document.addEventListener("DOMContentLoaded", function () {
   dicionario_cadastros();
+  dicionario_produto();
 });
 
 /* CRIAR DICIONARIO CADASTROS */
 function dicionario_cadastros() {
-  const cadastros = [
-    { id: 1, usuario: "Clarice", login: "Cla", senha: "1111" },
-    { id: 2, usuario: "Daniel", login: "Dan", senha: "2222" },
-    { id: 3, usuario: "Guilherme", login: "Gui", senha: "3333" },
-    { id: 4, usuario: "Mateus", login: "Mat", senha: "4444" },
-  ];
-  /* PASSAR PARA O FORMATO JSON */
-  const meujson = JSON.stringify(cadastros);
-  /* ARMAZENAR DADOS NO NAVEGADOR */
-  localStorage.setItem("bancodados", meujson);
+  if (!localStorage.getItem("bancodados")) {
+    const cadastros = [
+      { id: 1, usuario: "Clarice", login: "Cla", senha: "1111" },
+      { id: 2, usuario: "Daniel", login: "Dan", senha: "2222" },
+      { id: 3, usuario: "Guilherme", login: "Gui", senha: "3333" },
+      { id: 4, usuario: "Mateus", login: "Mat", senha: "4444" },
+    ];
+    /* PASSAR PARA O FORMATO JSON */
+    const meujson = JSON.stringify(cadastros);
+    /* ARMAZENAR DADOS NO NAVEGADOR */
+    localStorage.setItem("bancodados", meujson);
+  }
+}
+
+/* CRIAR DICIONARIO PRODUTOS */
+function dicionario_produtos() {
+  if (!localStorage.getItem("bancodados_produtos")) {
+    const produtos = [
+      {
+        id: "prod1",
+        nome: "Camiseta 1",
+        imagem: "assets/img/camiseta 1.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod2",
+        nome: "Camiseta 2",
+        imagem: "assets/img/camiseta 2.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod3",
+        nome: "Camiseta 3",
+        imagem: "assets/img/camiseta 3.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod4",
+        nome: "Camiseta 4",
+        imagem: "assets/img/camiseta 4.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod5",
+        nome: "Camiseta 5",
+        imagem: "assets/img/camiseta 5.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod6",
+        nome: "Camiseta 6",
+        imagem: "assets/img/camiseta 6.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod7",
+        nome: "Camiseta 7",
+        imagem: "assets/img/camiseta 7.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod8",
+        nome: "Camiseta 8",
+        imagem: "assets/img/camiseta 8.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+      {
+        id: "prod9",
+        nome: "Camiseta 9",
+        imagem: "assets/img/camiseta 9.png",
+        preco: 69.9,
+        alt: "Modelo com camiseta tematica do Superman",
+      },
+    ];
+    /* PASSAR PARA O FORMATO JSON */
+    const produtosJSON = JSON.stringify(produtos);
+    /* ARMAZENAR DADOS NO NAVEGADOR */
+    localStorage.setItem("bancodados_produtos", produtosJSON);
+  }
 }
 
 /* ADICIONADO FUNÇÃO DE REDIRECIONAR NOS BOTÕES DOS PRODUTOS */
